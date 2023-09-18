cf32c6e58fb6e8599b477a3af23025e7abcb744b
yian saechao <yian.l.saechao@gmail.com>
2023-09-17 22:28:09 -0700

    first commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavbarButton from '../Navbarbutton'
+
+
+const Navbar = () => {
+  return (
+    <NavbarButton/>
+  )
+}
+
+export default Navbar
---
+++ src/components/NavbarButton/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavbarButton = () => {
+  return (
+    <nav>
+        <button>Button One</button>
+        <button>Button Two</button>
+        <button>Button Three</button>
+        <button>Button Four</button>
+    </nav>
+  )
+}
+
+export default NavbarButton
